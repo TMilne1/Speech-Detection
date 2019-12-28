bf6b34030bbee2667afcbc0ad0c4c4d109d8b673
tmilne1 <tmilne1@gmail.com>
2019-12-28 14:36:25 -0500

    added speech recognition api, selected html elements to manipulate

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+/*To enable the SpeechRecognition in Firefox Nightly > 72, go to about:config and 
+switch the flags media.webspeech.recognition.enable and 
+media.webspeech.recognition.force_enable to true.
+*/
+
+window.SpeechRecognition = window.SpeechRecognition ||window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+ recognition.interimResults= true; // allows you to get result of speech currently as you are talking
+
+let p = document.createElement('p');
+const paragraph = document.querySelector('.words');
+paragraph.appendChild(p);
+
+function check(e){
+    console.log(e)
+    
+}
+recognition.addEventListener('end', recognition.start)  
+recognition.addEventListener('result', check)
+
+
+recognition.start();
